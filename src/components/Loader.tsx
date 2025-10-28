b189be51442e91040a32165dc3ff60214ff19b63
Soumyaranjan <s28252463@gmail.com>
2025-10-28 12:23:49 +0000

    can you please change only the loading page background with this

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,7 @@
 
 export function Loader() {
   return (
-    <div className="flex h-screen w-full items-center justify-center loader-background">
+    <div className="flex h-screen w-full items-center justify-center loader-background-animated">
       <div className="relative flex scale-[0.7] cursor-not-allowed items-center justify-center">
         <div className="loader-circle relative flex h-[10em] w-[10em] animate-[rotate_3s_linear_infinite] items-center justify-center rounded-full">
           <div className="loader-intern absolute z-[9999] text-white"></div>

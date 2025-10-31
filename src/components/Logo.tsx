565f5c400327a2e808889c661572e22ffa60d0f6
Soumyaranjan <s28252463@gmail.com>
2025-10-31 18:14:42 +0000

    no its totally different try agin

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,13 +8,13 @@ export function Logo() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <g>
-        {/* Green downward arrow */}
+        {/* Green downward arrow shape */}
         <path
           d="M45 10 L45 70 L25 70 L50 95 L75 70 L55 70 L55 10 Z"
           fill="#8CC63F"
           transform="skewX(-15)"
         />
-        {/* Blue upward arrow */}
+        {/* Blue upward arrow shape */}
         <path
           d="M55 90 L55 30 L75 30 L50 5 L25 30 L45 30 L45 90 Z"
           fill="#2C3E50"

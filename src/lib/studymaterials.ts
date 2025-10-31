7e39c105aca100647237fa28dc2d2fa0f203c8e0
Soumyaranjan <s28252463@gmail.com>
2025-10-31 06:55:19 +0000

    reverse the action you did a blunder

--- src/lib/studymaterials.ts
+++ src/lib/studymaterials.ts
@@ -8,18 +8,10 @@ export interface StudyMaterial {
 
 export const studyMaterialsData: { [topicId: string]: StudyMaterial } = {
   // --- PHYSICS ---
-  'p_bm': {
-    lectureUrl: "https://www.youtube.com/watch?v=hbga-xhCB4E",
-  },
-  'p_v': {
-    lectureUrl: "https://www.youtube.com/watch?v=Q4X2r-o0a_A",
-  },
-  'p_cm': {
-    lectureUrl: "https://www.youtube.com/watch?v=9nJqQdD-0jA",
-  },
-  'p_rm': {
-    lectureUrl: "https://www.youtube.com/watch?v=4fIuT5b9q9s",
-  },
+  'p_bm': {},
+  'p_v': {},
+  'p_cm': {},
+  'p_rm': {},
   'p1': {
     lectureUrl: "https://www.youtube.com/watch?v=J1Z2p4csb-w",
     notesUrl: "",

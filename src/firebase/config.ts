f5d790a62b9363f2b6516fd2dd49fa044d8b7510
Soumyaranjan <s28252463@gmail.com>
2025-11-01 15:37:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "neetrack-82i5b",
+  "appId": "1:440389918541:web:ffacf7c1ad70dfabaa2104",
+  "apiKey": "AIzaSyBNCJ4K3uuReVWRZOVVmyOxfubDJ8y_yYc",
+  "authDomain": "neetrack-82i5b.firebaseapp.com",
+  "measurementId": "G-83TE8QNRHY",
+  "messagingSenderId": "440389918541"
+};

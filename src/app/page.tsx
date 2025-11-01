93b1dc45d3b4d5502bf84edce201fb0baf11b9a4
Soumyaranjan <s28252463@gmail.com>
2025-11-01 16:54:03 +0000

    this is the design for loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser } from '@/firebase';
+import './loader.css';
 
 export default function HomePage() {
   const router = useRouter();
@@ -19,8 +20,62 @@ export default function HomePage() {
   }, [user, isUserLoading, router]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Loading...</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-white">
+      <div className="loader">
+        <div className="truckWrapper">
+          <div className="truckBody">
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 130 60"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M104.24,59.37H14.78a4,4,0,0,1-4-4V16.89a4,4,0,0,1,4-4H91.56a13.7,13.7,0,0,1,12.68,8.22L116,47.45V55.37a4,4,0,0,1-4,4ZM18.78,51.37H99.19v-26.H18.78Z"
+                fill="#282828"
+              ></path>
+              <path
+                d="M91.56,12.89h-76.78a4,4,0,0,0-4,4V29.11H99.19V21.11A8.22,8.22,0,0,0,91.56,12.89Z"
+                fill="#282828"
+              ></path>
+            </svg>
+          </div>
+          <div className="truckTires">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="12" cy="12" r="10" fill="#282828"></circle>
+              <circle cx="12" cy="12" r="4" fill="white"></circle>
+            </svg>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="12" cy="12" r="10" fill="#282828"></circle>
+              <circle cx="12" cy="12" r="4" fill="white"></circle>
+            </svg>
+          </div>
+          <div className="road"></div>
+          <svg
+            className="lampPost"
+            width="40"
+            height="90"
+            viewBox="0 0 40 90"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M5 90V10H35V0L25 10H5" stroke="#282828" strokeWidth="3" />
+          </svg>
+        </div>
+      </div>
     </main>
   );
 }

061bff8df09652497e947498808ee82f83295a94
Soumyaranjan <s28252463@gmail.com>
2025-11-01 01:47:54 +0000

    yes we will work on loading page . i am providing you a css code for the

---
+++ src/app/loading.tsx
@@ -0,0 +1,48 @@
+import './loader.css';
+
+export default function Loading() {
+  return (
+    <main className="main">
+      <div className="loaders">
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+        <div className="loader"></div>
+      </div>
+      <div className="loadersB">
+        <div className="loaderA">
+          <div className="ball0"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball1"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball2"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball3"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball4"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball5"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball6"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball7"></div>
+        </div>
+        <div className="loaderA">
+          <div className="ball8"></div>
+        </div>
+      </div>
+    </main>
+  );
+}

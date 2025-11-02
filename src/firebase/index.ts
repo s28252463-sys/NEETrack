e64a412cbd0cf3810988fb43d9bc43785a48000c
Soumyaranjan <s28252463@gmail.com>
2025-11-02 07:52:39 +0000

    still not loading

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -16,6 +16,7 @@ export function initializeFirebase() {
     try {
       // Attempt to initialize via Firebase App Hosting environment variables
       firebaseApp = initializeApp();
+      return getSdks(firebaseApp);
     } catch (e) {
       // Only warn in production because it's normal to use the firebaseConfig to initialize
       // during development
@@ -23,9 +24,8 @@ export function initializeFirebase() {
         console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
       }
       firebaseApp = initializeApp(firebaseConfig);
+      return getSdks(firebaseApp);
     }
-
-    return getSdks(firebaseApp);
   }
 
   // If already initialized, return the SDKs with the already initialized App

69c5eaca7933bbe3af060714a4f409f587af16d0
Soumyaranjan <s28252463@gmail.com>
2025-11-05 06:51:54 +0000

    i want this image in background of only the pomodoro timer section

--- src/app/(app)/pomodoro-timer/page.tsx
+++ src/app/(app)/pomodoro-timer/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import PomodoroTimer from '@/components/pomodoro-timer';
+import Image from 'next/image';
 
 export default function PomodoroTimerPage() {
   return (
-    <div className="dark -m-8 flex flex-1 justify-center items-start p-8">
-      <PomodoroTimer />
+    <div className="dark -m-8 flex flex-1 justify-center items-start p-8 relative">
+       <Image
+        src="/pomodoro-bg.jpg"
+        alt="Focus background"
+        layout="fill"
+        objectFit="cover"
+        className="z-0 opacity-20"
+      />
+      <div className="z-10 flex flex-1 justify-center items-start">
+        <PomodoroTimer />
+      </div>
     </div>
   );
 }

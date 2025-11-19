a5d7de7348852b41b43728de7c5295517758496b
Soumyaranjan <s28252463@gmail.com>
2025-11-19 18:48:37 +0000

    now add a button to install the app and when users click on it the app s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'NEETrack',
@@ -22,7 +21,6 @@ export default function RootLayout({
           {children}
         </FirebaseClientProvider>
         <Toaster />
-        <Script async={true} data-cfasync="false" src="//pl28085801.effectivegatecpm.com/48a2c7d50ab69852baf22279c014a0b5/invoke.js"></Script>
       </body>
     </html>
   );

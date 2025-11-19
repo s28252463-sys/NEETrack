bf70af5e0011de7904b832b63c926cb114248f25
Soumyaranjan <s28252463@gmail.com>
2025-11-19 05:48:31 +0000

    also add this to bottom of the pomodoro page

--- src/app/(app)/pomodoro-timer/page.tsx
+++ src/app/(app)/pomodoro-timer/page.tsx
@@ -5,11 +5,12 @@ import { PomodoroBackground } from '@/components/ui/pomodoro-background';
 
 export default function PomodoroTimerPage() {
   return (
-    <div className="-m-8 flex flex-1 justify-center items-start p-8 relative overflow-hidden theme-serenity">
+    <div className="-m-8 flex flex-1 flex-col justify-between items-center p-8 relative overflow-hidden theme-serenity">
       <PomodoroBackground />
-      <div className="z-10 flex flex-1 justify-center items-start">
+      <div className="z-10 flex flex-1 justify-center items-center w-full">
         <PomodoroTimer />
       </div>
+      <div id="container-48a2c7d50ab69852baf22279c014a0b5" className="z-10 mt-8"></div>
     </div>
   );
 }

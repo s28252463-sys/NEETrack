9c9c8033314704171af4af6decfad0f655252a7f
Soumyaranjan <s28252463@gmail.com>
2025-11-19 05:50:49 +0000

    also add this to the bottom of the mock test page

--- src/app/(app)/mock-tests/page.tsx
+++ src/app/(app)/mock-tests/page.tsx
@@ -4,8 +4,9 @@ import MockTestTracker from '@/components/mock-test-tracker';
 
 export default function MockTestsPage() {
   return (
-    <div className="theme-thunder -m-8 flex flex-1 justify-center items-start p-8">
+    <div className="theme-thunder -m-8 flex flex-1 flex-col justify-start items-center p-8 gap-8">
       <MockTestTracker />
+      <div id="container-48a2c7d50ab69852baf22279c014a0b5"></div>
     </div>
   );
 }
